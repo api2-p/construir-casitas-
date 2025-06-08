ae81ed7a2f38f5b9d7c7e8b7000b68fd6393872a
Erika <johannita.1125@gmail.com>
2025-06-07 20:41:58 -0500

    ok

--- frontendgatuno/vite.config.js
+++ frontendgatuno/vite.config.js
@@ -1,18 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ✅ Esto asegura que las rutas funcionen en producción (Render)
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
